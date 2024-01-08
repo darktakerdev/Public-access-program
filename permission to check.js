59c1a9ab49e5e37fade22ff1262dfb52256e75e4
Andy Rubbin <155994875+darktakerdev@users.noreply.github.com>
2024-01-08 18:00:32 +0200

    Create permission to check.js

---
+++ permission to check.js
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom/extend-expect'
+import { setConfig } from 'next/config'
+import config from './next.config'
+
+const { compilerOptions } = require('./tsconfig.json')
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/@next/ApiReq/*',
+    '!**/@next/utils/*',
+    '!**/@next/hooks/*',
+    '!**/@next/store/slices/auth/*',
+    '!**/*.d.ts'
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/\\.next/'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+        https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    '^.+\\.(js|jsx|ts|tsx|svg)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
